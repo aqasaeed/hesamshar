7e2cbd2d916df07024320fd6d017dd8243c51c92
aqasaeed <saeidzarein@yahoo.com>
2016-01-14 10:12:58 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,7 +223,7 @@ function create_config( )
     },
     sudo_users = {127638799},--Sudo users
     disabled_channels = {},
-    moderation = {data = 'data/moderation.json'},
+    moderation = {70704241},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
